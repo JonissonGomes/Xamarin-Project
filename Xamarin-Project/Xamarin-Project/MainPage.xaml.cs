2b9cd44b84457315e1de0cbec9efba53b410cfb0
JonissonGomes <joni_gomes@hotmail.com>
2021-04-06 23:56:16 -0300

    Subindo arquivos do projeto welcome user

---
+++ Xamarin-Project/Xamarin-Project/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_Project
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Mostrar(object sender, EventArgs e){
+
+            var Username = username.Text;
+            response.Text = "Bem vindo, " + username.Text + "!";
+        }
+    }
+}
